ea08ba3efe20a0ecfc09e386851e9ca61dfaddbc
anvitha305 <44482134+anvitha305@users.noreply.github.com>
2022-06-27 14:00:19 -0500

    Update run_exercise.py

--- utils/run_exercise.py
+++ utils/run_exercise.py
@@ -27,7 +27,6 @@ from time import sleep
 
 import p4runtime_lib.simple_controller
 from mininet.cli import CLI
-from mininet.link import TCLink
 from mininet.net import Mininet
 from mininet.topo import Topo
 from p4_mininet import P4Host, P4Switch
@@ -254,7 +253,6 @@ class ExerciseRunner:
         self.topo = ExerciseTopo(self.hosts, self.switches, self.links, self.log_dir, self.bmv2_exe, self.pcap_dir)
 
         self.net = Mininet(topo = self.topo,
-                      link = TCLink,
                       host = P4Host,
                       switch = defaultSwitchClass,
                       controller = None)
